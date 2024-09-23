62c840c072743069c49417c7d2450c5c9daceabf
Seraquesada <quesada.serafin@gmail.com>
2024-09-23 15:03:30 -0300

    adding freelance works

--- src/components/works.tsx
+++ src/components/works.tsx
@@ -17,7 +17,7 @@ const Works: FC = () => {
 								Upler - Front End Developer
 							</h4>
 							<span className="text-xl font-light ">
-								08/2023 - Current
+								08/2023 - 04/2024
 							</span>
 						</div>
 
@@ -44,14 +44,53 @@ const Works: FC = () => {
 							</Link>
 							<Link
 								className="font-semibold uppercase hover:text-cyan-500"
-								href="https://upler.tech/"
+								href="https://github.com/UplerSolutions/UplerSolutions.github.io"
 								target="_blamk"
 							>
 								Git Hub
 							</Link>
 						</div>
 					</div>
-					<div className="relative  mx-auto duration-300 hover:opacity-60 ">
+					<div className="flex flex-col gap-2  md:w-3/5 ">
+						<div className="flex flex-col  gap-3 2xl:flex-row 2xl:items-end">
+							<h4 className="text-3xl font-semibold ">
+								Materia Prima - Freelance - Front End Developer
+							</h4>
+							<span className="text-xl font-light ">
+								06/2024 - 09/2024
+							</span>
+						</div>
+
+						<p className="w-4/5  text-lg">
+							Project made as a freelancer for the company Materia
+							Prima. It is mainly a web for people to look at the
+							job they make and contact them.
+						</p>
+						<p>
+							<span className="font-semibold">
+								Technologic Stack:{' '}
+							</span>
+							Next Js, TypeScript, Tailwind and GSAP.
+						</p>
+						<div className="flex gap-5">
+							<Link
+								className="font-semibold uppercase hover:text-cyan-500"
+								href="https://materia-prima.vercel.app/"
+								target="_blamk"
+							>
+								web site
+							</Link>
+							<Link
+								className="font-semibold uppercase hover:text-cyan-500"
+								href="https://github.com/Seraquesada/Materia-Prima"
+								target="_blamk"
+							>
+								Git Hub
+							</Link>
+						</div>
+					</div>
+
+					{/* <div className="relative  mx-auto duration-300 hover:opacity-60 ">
 						<Image
 							className="inset-0 z-0 rounded-md  bg-cover bg-center"
 							src={'/upler.jpeg'}
@@ -62,7 +101,7 @@ const Works: FC = () => {
 						<div className="absolute inset-0 z-10 flex items-center justify-center text-3xl font-bold text-[#f9f9f9]  opacity-0 duration-300 hover:opacity-100 ">
 							Upler @ ETH ARG 2023
 						</div>
-					</div>
+					</div> */}
 				</div>
 			</div>
 		</section>
